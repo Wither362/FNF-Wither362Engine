1603335471947dea7a84ba3664681be38bc4032e
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-02-22 08:52:54 +0100

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -112,7 +112,20 @@ class FreeplayState extends MusicBeatState
 			songText.isMenuItem = true;
 			songText.targetY = i;
 			grpSongs.add(songText);
-
+			
+			if (songText.width > 980)
+ 			{
+ 				var textScale:Float = 980 / songText.width;
+ 				songText.scale.x = textScale;
+ 				for (letter in songText.lettersArray)
+ 				{
+ 					letter.x *= textScale;
+ 					letter.offset.x *= textScale;
+ 				}
+ 				//songText.updateHitbox();
+ 				//trace(songs[i].songName + ' new scale: ' + textScale);
+ 			}
+			
 			Paths.currentModDirectory = songs[i].folder;
 			var icon:HealthIcon = new HealthIcon(songs[i].songCharacter);
 			icon.sprTracker = songText;
