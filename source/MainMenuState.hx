13187d800f29131d98476b41d50e2a163700715c
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-02-22 09:59:58 +0100

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -49,8 +49,6 @@ class MainMenuState extends MusicBeatState
 
 	override function create()
 	{
-		WeekData.loadTheFirstEnabledMod();
-		
 		#if desktop
 		// Updating Discord Rich Presence
 		DiscordClient.changePresence("In the Menus", null);
