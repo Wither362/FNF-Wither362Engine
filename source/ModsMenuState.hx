ae4835229b8811176093442e223adbfbeaf684bb
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-02-22 10:01:07 +0100

    Update ModsMenuState.hx

--- source/ModsMenuState.hx
+++ source/ModsMenuState.hx
@@ -300,8 +300,7 @@ class ModsMenuState extends MusicBeatState
 			var newMod:ModMetadata = new ModMetadata(values[0]);
 			mods.push(newMod);
 
-			newMod.alphabet = new Alphabet(0, 0, mods[i].name, true, false, 0.05);
- 			var scale:Float = Math.min(840 / newMod.alphabet.width, 1);
+			var scale:Float = Math.min(16 / (mods[i].name.length), 1);
 			newMod.alphabet = new Alphabet(0, 0, mods[i].name, true, false, 0.05, scale);
 			newMod.alphabet.y = i * 150;
 			newMod.alphabet.x = 310;
