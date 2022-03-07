373ddb451afcdd33a937bdb600e9784a840f4436
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-03-07 10:43:43 +0100

    Update OutdatedState.hx

--- source/OutdatedState.hx
+++ source/OutdatedState.hx
@@ -24,11 +24,11 @@ class OutdatedState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Sup bro, looks like you're running an   \n
-			outdated version of Psych Engine (" + MainMenuState.psychEngineVersion + "),\n
+			"Fucking shit bro, looks like you're running an   \n
+			fucking outdated version of Wither Engine (" + MainMenuState.psychEngineVersion + "),\n
 			please update to " + TitleState.updateVersion + "!\n
 			\n
-			Thank you for using the Engine!",
+			Fucking thank you for using the Engine!",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
