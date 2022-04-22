d46984305f6979a447a4c5d6274a773a86dd68c6
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-04-22 10:24:26 +0200

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -35,7 +35,7 @@ class MainMenuState extends MusicBeatState
 	var optionShit:Array<String> = [
 		'story_mode',
 		'freeplay',
-                #if !moreThings 'mods', #end
+		if (ClientPrefs.moreThings == true) { 'mods',}
 		#if ACHIEVEMENTS_ALLOWED 'awards', #end
 		'credits',
 		#if !switch 'donate', #end
