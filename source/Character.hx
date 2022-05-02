f1460dc14132c417551be12c334a419150dda473
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-02 10:22:12 +0200

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -9,6 +9,7 @@ import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxSort;
 import Section.SwagSection;
+import lime.utils.Assets;
 #if MODS_ALLOWED
 import sys.io.File;
 import sys.FileSystem;
